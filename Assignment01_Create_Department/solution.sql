5b85a0424d7cb24d4cbacd9f887bf2c2758c3d19
1U25CA011-ANUPRIYA <anupriyas5039@gmail.com>
2026-09-11 09:45:07 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,4 +1,6 @@
--- Write your SQL here
+Create database Anupriya;
+Use Anupriya;
+Create table department 3(dept id int(5)PRIMARY KEY,dept name varchar(20),HOD varchar(20));desc department 3;-- Write your SQL here
 
 -- Create Database
 
